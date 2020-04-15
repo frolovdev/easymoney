0a6609582343e86b33d75c45ecfca7480eefa7bb
Whispers12 <frolovandrey752@gmail.com>
2020-04-15 10:47:41 +0400

    build: add algolia search

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -8,42 +8,47 @@ module.exports = {
   organizationName: "whispers12", // Usually your GitHub org/user name.
   projectName: "easymoney", // Usually your repo name.
   themeConfig: {
+    algolia: {
+      indexName: "easymoney",
+      apiKey: process.env.ALGOLIA_API_KEY,
+    },
+
     sidebarCollapsible: false,
     prism: {
       theme: require("prism-react-renderer/themes/vsDark"),
-      darkTheme: require("prism-react-renderer/themes/vsDark")
+      darkTheme: require("prism-react-renderer/themes/vsDark"),
     },
 
     navbar: {
       title: "easymoney",
       logo: {
         alt: "My Site Logo",
-        src: "img/logo.png"
+        src: "img/logo.png",
       },
       links: [
         {
           to: "docs/introduction/getting-started",
           activeBasePath: "docs",
           label: "Docs",
-          position: "left"
+          position: "left",
         },
         { to: "docs/api/api-reference", label: "Api", position: "left" },
         {
           to: "docs/introduction/getting-started#help-and-discussion",
           label: "Need help?",
-          position: "right"
+          position: "right",
         },
         {
           href: "https://spectrum.chat/easymoney",
           label: "Spectrum",
-          position: "right"
+          position: "right",
         },
         {
           href: "https://github.com/frolovdev/easymoney",
           label: "GitHub",
-          position: "right"
-        }
-      ]
+          position: "right",
+        },
+      ],
     },
     footer: {
       style: "dark",
@@ -53,43 +58,43 @@ module.exports = {
           items: [
             {
               label: "Getting started",
-              to: "docs/introduction/getting-started"
+              to: "docs/introduction/getting-started",
             },
             {
               label: "FAQ",
-              to: "docs/introduction/getting-started"
+              to: "docs/introduction/getting-started",
             },
             {
               label: "Tutorial",
-              to: "docs/introduction/getting-started"
+              to: "docs/introduction/getting-started",
             },
             {
               label: "API Reference",
-              to: "docs/api/api-reference"
-            }
-          ]
+              to: "docs/api/api-reference",
+            },
+          ],
         },
         {
           title: "Community",
           items: [
             {
               label: "Spectrum",
-              href: "https://spectrum.chat/easymoney"
-            }
-          ]
+              href: "https://spectrum.chat/easymoney",
+            },
+          ],
         },
         {
           title: "Social",
           items: [
             {
               label: "GitHub",
-              href: "https://github.com/frolovdev/easymoney"
-            }
-          ]
-        }
+              href: "https://github.com/frolovdev/easymoney",
+            },
+          ],
+        },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} frolovdev. Built with Docusaurus.`
-    }
+      copyright: `Copyright © ${new Date().getFullYear()} frolovdev. Built with Docusaurus.`,
+    },
   },
   presets: [
     [
@@ -97,12 +102,12 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          editUrl: "https://github.com/frolovdev/easymoney/tree/master/website"
+          editUrl: "https://github.com/frolovdev/easymoney/tree/master/website",
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css")
-        }
-      }
-    ]
-  ]
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
 };
