6c22fd245aeedf5163fc64d92c152ab2e69fae49
Whispers12 <frolovandrey752@gmail.com>
2020-04-18 11:57:07 +0400

    fix: add base crypto fromatting

---
+++ packages/formatter/src/__tests__/createMoneyCryptoFormatter.test.ts
@@ -0,0 +1,54 @@
+import { createMoneyCryptoFormatterUnit } from "../createMoneyCryptoFormatter";
+import { cryptoCurrencies } from "@easymoney/currencies";
+import { MoneyIntlFormatter } from "../types";
+import { createMoney } from "@easymoney/money";
+
+describe("createMoneyIntlFormatter", () => {
+  let createFormatter: () => MoneyIntlFormatter;
+  beforeAll(() => {
+    createFormatter = createMoneyCryptoFormatterUnit(cryptoCurrencies);
+  });
+
+  describe("format", () => {
+    it("should throw an error if currency doesn't exist", () => {
+      const money = { amount: 5, currency: "XBT" };
+
+      const expression = () => createFormatter().format(createMoney(money));
+      expect(expression).toThrow();
+    });
+    it("should format valid ", () => {
+      const money = { amount: 5, currency: "LTC" };
+
+      const formattedValue = createFormatter().format(createMoney(money));
+      expect(formattedValue).toEqual("$0.05");
+    });
+
+    // it("should valid format and merge options", () => {
+    //   const money = { amount: 50, currency: "USD" };
+
+    //   const formattedValue = createFormatter().format(
+    //     createMoney(money),
+    //     "en-US",
+    //     { minimumFractionDigits: 1, maximumFractionDigits: 1 }
+    //   );
+    //   expect(formattedValue).toEqual("$0.5");
+    // });
+
+    // it("should valid format ", () => {
+    //   const money = { amount: 500, currency: "USD" };
+
+    //   const formattedValue = createFormatter().format(createMoney(money));
+    //   expect(formattedValue).toEqual("$5.00");
+    // });
+
+    // it("should be possible pass different locales", () => {
+    //   const money = { amount: 500, currency: "USD" };
+
+    //   const formattedValue = createFormatter().format(
+    //     createMoney(money),
+    //     "ru-RU"
+    //   );
+    //   expect(formattedValue).toEqual("$5.00");
+    // });
+  });
+});
--- packages/formatter/src/createMoneyCryptoFormatter.ts
+++ packages/formatter/src/createMoneyCryptoFormatter.ts
@@ -2,39 +2,91 @@ import { bind } from "@easymoney/core";
 import {
   createCurrencyList,
   CurrencyList,
-  CurrencyUnitISO,
+  CurrencyUnitCrypto
 } from "@easymoney/currencies";
 import { MoneyIntlOptions, MoneyIntlFormatter } from "./types";
 import { MoneyBase } from "@easymoney/money";
 import { lpad } from "./lpad";
 
 type PrivateInstance = {
-  currencyList: CurrencyList<CurrencyUnitISO>;
+  currencyList: CurrencyList<CurrencyUnitCrypto>;
 };
 
-export function createIntlFormatterFactory(
-  currencyList: CurrencyList<CurrencyUnitISO>
+export function createCryptoFormatterFactory(
+  currencyList: CurrencyList<CurrencyUnitCrypto>
 ) {
   const privateInstance = { currencyList };
 
   const publicInstance = {
-    format: bind(format, privateInstance),
+    format: bind(format, privateInstance)
   } as MoneyIntlFormatter;
 
   return publicInstance;
 }
 
-export function createMoneyIntlFormatterUnit(currencies: CurrencyUnitISO[]) {
+export function createMoneyCryptoFormatterUnit(
+  currencies: CurrencyUnitCrypto[]
+) {
   const currencyList = createCurrencyList(currencies);
 
-  return createIntlFormatterFactory.bind(null, currencyList);
+  return createCryptoFormatterFactory.bind(null, currencyList);
 }
 
 const defaultOptions: MoneyIntlOptions = {
   currencyDisplay: "symbol",
   useGrouping: true,
-  style: "currency",
+  style: "currency"
 };
+
+// function format(
+//   privateInstance: PrivateInstance,
+//   money: MoneyBase,
+//   locale: string = "en-US",
+//   options: MoneyIntlOptions
+// ) {
+//   const mergedOptions = { ...defaultOptions, ...options };
+//   let valueBase = money.getAmount();
+//   let negative = false;
+
+//   if (valueBase[0] === "-") {
+//     negative = true;
+//     valueBase = valueBase.slice(1);
+//   }
+
+//   const subunit = privateInstance.currencyList.subUnitFor(money.getCurrency());
+//   const valueLength = valueBase.length;
+
+//   let formatted: string;
+//   let decimalDigitsLength;
+//   if (valueLength > subunit) {
+//     formatted = valueBase.slice(0, valueLength - subunit);
+//     const decimalDigits = valueBase.slice(valueLength - subunit);
+
+//     decimalDigitsLength = decimalDigits.length;
+//     if (decimalDigitsLength > 0) {
+//       formatted = `${formatted}.${decimalDigits}`;
+//     }
+//   } else {
+//     const zeros = lpad("", "0", subunit - valueLength);
+//     formatted = `0.${zeros}${valueBase}`;
+//   }
+
+//   if (negative === true) {
+//     formatted = `-${formatted}`;
+//   }
+
+//   const currency = money.getCurrency();
+//   return Number(formatted).toLocaleString(locale, {
+//     currency: typeof currency === "object" ? currency.code : currency,
+//     useGrouping: mergedOptions.useGrouping,
+//     style: mergedOptions.style,
+//     currencyDisplay: mergedOptions.currencyDisplay,
+//     minimumFractionDigits:
+//       mergedOptions.minimumFractionDigits || decimalDigitsLength,
+//     maximumFractionDigits:
+//       mergedOptions.maximumFractionDigits || decimalDigitsLength,
+//   });
+// }
 
 function format(
   privateInstance: PrivateInstance,
@@ -82,6 +134,6 @@ function format(
     minimumFractionDigits:
       mergedOptions.minimumFractionDigits || decimalDigitsLength,
     maximumFractionDigits:
-      mergedOptions.maximumFractionDigits || decimalDigitsLength,
+      mergedOptions.maximumFractionDigits || decimalDigitsLength
   });
 }
