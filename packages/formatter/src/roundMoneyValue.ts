6014d798a5eb5ab32cfdf6aadf8fbc958192865a
Whispers12 <frolovandrey752@gmail.com>
2020-04-22 19:07:32 +0400

    fix: delete reference

--- packages/formatter/src/roundMoneyValue.ts
+++ packages/formatter/src/roundMoneyValue.ts
@@ -1,47 +1,5 @@
 import { setCharAt } from "./setChar";
 
-//  /**
-//      * @param string $moneyValue
-//      * @param int    $targetDigits
-//      * @param int    $havingDigits
-//      *
-//      * @return string
-//      */
-//     public static function roundMoneyValue($moneyValue, $targetDigits, $havingDigits)
-//     {
-//         $valueLength = strlen($moneyValue);
-//         $shouldRound = $targetDigits < $havingDigits && $valueLength - $havingDigits + $targetDigits > 0;
-
-//         if ($shouldRound && $moneyValue[$valueLength - $havingDigits + $targetDigits] >= 5) {
-//             $position = $valueLength - $havingDigits + $targetDigits;
-//             $addend = 1;
-
-//             while ($position > 0) {
-//                 $newValue = (string) ((int) $moneyValue[$position - 1] + $addend);
-
-//                 if ($newValue >= 10) {
-//                     $moneyValue[$position - 1] = $newValue[1];
-//                     $addend = $newValue[0];
-//                     --$position;
-//                     if ($position === 0) {
-//                         $moneyValue = $addend.$moneyValue;
-//                     }
-//                 } else {
-//                     if ($moneyValue[$position - 1] === '-') {
-//                         $moneyValue[$position - 1] = $newValue[0];
-//                         $moneyValue = '-'.$moneyValue;
-//                     } else {
-//                         $moneyValue[$position - 1] = $newValue[0];
-//                     }
-
-//                     break;
-//                 }
-//             }
-//         }
-
-//         return $moneyValue;
-//     }
-
 export function roundMoneyValue(
   moneyValue: string,
   targetDigits: number,
