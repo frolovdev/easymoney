ef6c403f69b3dff02fb3791eb63d1ba0703e6f97
Whispers12 <frolovandrey752@gmail.com>
2020-04-22 19:06:01 +0400

    fix: delete comments

--- packages/formatter/src/createMoneyCryptoFormatter.ts
+++ packages/formatter/src/createMoneyCryptoFormatter.ts
@@ -36,62 +36,9 @@ export function createMoneyCryptoFormatterUnit(
 }
 
 const defaultOptions: CryptoOptions = {
-  // currencyDisplay: "symbol",
-  // useGrouping: true,
-  // style: "currency"
   space: false,
   currencyPosition: 1
 };
-
-// function format(
-//   privateInstance: PrivateInstance,
-//   money: MoneyBase,
-//   locale: string = "en-US",
-//   options: MoneyIntlOptions
-// ) {
-//   const mergedOptions = { ...defaultOptions, ...options };
-//   let valueBase = money.getAmount();
-//   let negative = false;
-
-//   if (valueBase[0] === "-") {
-//     negative = true;
-//     valueBase = valueBase.slice(1);
-//   }
-
-//   const subunit = privateInstance.currencyList.subUnitFor(money.getCurrency());
-//   const valueLength = valueBase.length;
-
-//   let formatted: string;
-//   let decimalDigitsLength;
-//   if (valueLength > subunit) {
-//     formatted = valueBase.slice(0, valueLength - subunit);
-//     const decimalDigits = valueBase.slice(valueLength - subunit);
-
-//     decimalDigitsLength = decimalDigits.length;
-//     if (decimalDigitsLength > 0) {
-//       formatted = `${formatted}.${decimalDigits}`;
-//     }
-//   } else {
-//     const zeros = lpad("", "0", subunit - valueLength);
-//     formatted = `0.${zeros}${valueBase}`;
-//   }
-
-//   if (negative === true) {
-//     formatted = `-${formatted}`;
-//   }
-
-//   const currency = money.getCurrency();
-//   return Number(formatted).toLocaleString(locale, {
-//     currency: typeof currency === "object" ? currency.code : currency,
-//     useGrouping: mergedOptions.useGrouping,
-//     style: mergedOptions.style,
-//     currencyDisplay: mergedOptions.currencyDisplay,
-//     minimumFractionDigits:
-//       mergedOptions.minimumFractionDigits || decimalDigitsLength,
-//     maximumFractionDigits:
-//       mergedOptions.maximumFractionDigits || decimalDigitsLength,
-//   });
-// }
 
 function format(
   privateInstance: PrivateInstance,
@@ -120,7 +67,7 @@ function format(
   const valueLength = valueBase.length;
 
   let formatted: string;
-  // let decimalDigitsLength;
+
   if (valueLength > subunit) {
     formatted = valueBase.slice(0, valueLength - subunit);
 
