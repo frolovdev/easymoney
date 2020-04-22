073cfcf13038cbca0b0d8c4262bff28b150224fd
Whispers12 <frolovandrey752@gmail.com>
2020-04-22 19:06:24 +0400

    fix: delete trashy code

--- packages/formatter/src/__tests__/roundMoneyValue.test.ts
+++ packages/formatter/src/__tests__/roundMoneyValue.test.ts
@@ -1,6 +1,5 @@
 import { roundMoneyValue } from "../roundMoneyValue";
 
-// $valueBase = Number::roundMoneyValue($valueBase, $this->fractionDigits, $subunit);
 describe("roundMoneyValue", () => {
   it("should works)", () => {
     expect(roundMoneyValue("100", 2, 2));
