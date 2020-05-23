f7e007a97e86a5fa6466046c08f0ec0deaf96cc9
Whispers12 <frolovandrey752@gmail.com>
2020-05-23 13:32:16 +0400

    ci: update jest configs

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,10 @@
 const base = require("./jest.config.base.js");
+
 module.exports = {
-  // p: ["<rootDir>/packages/*"],
   ...base,
   roots: ["<rootDir>"],
-  projects: ["<rootDir>/packages/*/jest.config.js"],
-  // moduleNameMapper: {
-  //   "@easymoney/(.*)$": "<rootDir>/packages/$1",
-  // },
-  // preset: "ts-jest",
-  // testEnvironment: "node",
-  // globals: {
-  //   "ts-jest": {
-  //     tsConfig: "tsconfig.json",
-  //   },
-  // },
-  // transform: {
-  //   "^.+\\.tsx?$": "ts-jest",
-  // },
-
-  // modulePathIgnorePatterns: ["dist"],
-  // testMatch: [`**/__tests__/*.test.ts`, `**/__tests__/*.propbased-test.ts`],
+  projects: [
+    "<rootDir>/packages/*/jest.config.js",
+    "<rootDir>/integration/jest.config.js"
+  ]
 };
